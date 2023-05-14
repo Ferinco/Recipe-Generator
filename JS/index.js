8fe16a5e26e40a5344632500f6c486e8e28ade4f
Ferinco <emmanuelferrum003@gmail.com>
2023-05-14 10:56:57 +0100

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+const frontPage = document.getElementById("front-page")
+const preLoader = document.getElementById("preloader")
+window.addEventListener("load",()=>{
+frontPage.style.display = "block"
+preLoader.style.display = "none"
+})
