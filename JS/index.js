527ee2ea46b741b31108f64d0f3eaea747682094
Ferinco <emmanuelferrum003@gmail.com>
2023-05-16 00:08:48 +0100

    initial commit

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,20 @@
 const frontPage = document.getElementById("front-page")
 const preLoader = document.getElementById("preloader")
+const menuBar = document.getElementById("menu-bar")
+const cancelBar = document.getElementById("cancel-bar")
+// cancelBar.style.display = "none"
+
 frontPage.style.display = "none"
 window.addEventListener("load",()=>{
 frontPage.style.display = "block"
 preLoader.style.display = "none"
+})
+menuBar.addEventListener("click",()=>{
+    menuBar.style.display= "none"
+    cancelBar.style.marginTop = "-10px"
+cancelBar.style.display = "flex"
+})
+cancelBar.addEventListener("click",()=>{
+    menuBar.style.display = "flex"
+    cancelBar.style.display = "none"
 })
