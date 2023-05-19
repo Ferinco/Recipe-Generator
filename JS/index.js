5ab88b9c4f7e55cc82a6cbf9288a29a7bbf521a2
Ferinco <emmanuelferrum003@gmail.com>
2023-05-19 03:44:32 +0100

    image fetch

--- JS/index.js
+++ JS/index.js
@@ -19,7 +19,7 @@ searchBtn.addEventListener("click",(e)=>{
     .then(data =>{
         console.log(data)
         if(data.meals){
-   
+    document.querySelector(".results-header").style.display = "none"
     data.meals.forEach(meal => {
         searchResults.innerHTML = ""
     const resultMeal = document.createElement("div")
@@ -30,40 +30,17 @@ searchBtn.addEventListener("click",(e)=>{
             <img src="${meal.strMealThumb}" alt="" />
           </div>
           <div class="result-meal-name"><h3>${meal.strMeal}</h3></div>
-          <div class="result-meal-button"><button  onclick ="getMealRecipe()">details</button></div>
+          <div class="result-meal-button"><button class="details-btn" onclick = "getMealRecipe()">details</button></div>
         `
         
         result.appendChild(resultMeal)
         searchResults.appendChild(result)
-        getMealRecipe=()=>{
-            fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${resultMeal.dataset.id}`)
-            .then(response =>response.json())
-            .then(data =>{
-                console.log(data);
-                 
-                recipe = document.createElement("div")
-                recipe.className = "recipe"
-                recipe.innerHTML = `
-                <div class="recipe-button">
-                <button id="close-recipe"></button>
-              </div>
-              <div class="recipe-body">
-                <h3>${meal.strMeal}</h3>
-                <p>Category</p>
-                <h3>Instructions</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, officiis. Consequatur deleniti mollitia vitae culpa aliquid tenetur expedita optio? Odio enim ipsam veritatis. Excepturi et consectetur eum, aut quia laboriosam quisquam ad omnis cupiditate deleniti blanditiis vel, reprehenderit ullam soluta.</p>
-              </div>`
-              searchResults.appendChild(recipe)
-              console.log(recipe)
-                
-            })
-        }
     });
 }
 else{
     document.querySelector(".results-header").style.display = "none"
-notFound = document.createElement("p")
-notFound.id = "not-found"
+    notFound = document.createElement("p")
+    notFound.id = "not-found"
 notFound.innerHTML = `sorry, couldn't find a match or your search`
 result.appendChild(notFound)
 searchResults.appendChild(result)
@@ -76,6 +53,46 @@ result.style.alignItems = "center"
 .catch(error =>{console.log(error)})
 })
 
+function getMealRecipe(e){
+    // e.preventDefault();
+    // if (e.target.classList.contains("details-btn")) {
+        let resultMeal = document.querySelector(".result-meal")
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${resultMeal.dataset.id}`)
+        .then(response =>response.json())
+        .then(data =>{
+            console.log(data);
+            let meal = meal[0];
+            recipe = document.createElement("div")
+            recipe.className = "recipe"
+            recipe.innerHTML = `
+            <div class="recipe-button">
+            <button id="close-recipe"></button>
+            </div>
+            <div class="recipe-body">
+            <h3>${meal.strMeal}</h3>
+            <p>Category</p>
+            <h3>Instructions</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, officiis. Consequatur deleniti mollitia vitae culpa aliquid tenetur expedita optio? Odio enim ipsam veritatis. Excepturi et consectetur eum, aut quia laboriosam quisquam ad omnis cupiditate deleniti blanditiis vel, reprehenderit ullam soluta.</p>
+            </div>`
+            searchResults.appendChild(recipe)
+            console.log(recipe)
+            
+        })
+    // }
+}
+window.onload = displayFoods=()=>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52773`)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        if(data.meals){
+            const foodLink = document.createElement("a");
+            foodLink.innerHTML = `
+            <img src="${data.meals[0].strMealThumb}" alt=""/>`
+            document.getElementById("first").appendChild(foodLink)
+        }
+    })
+}
 
 
 frontPage.style.display = "none"
